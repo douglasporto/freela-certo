f8a8e1e5deb8549ef3fcd02dd2e1741718159f4b
Douglas Porto <douglasalexandre7@gmail.com>
2021-08-10 16:06:22 -0300

    :bulb: Chore(storybook): config storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,26 @@
+const path = require('path')
+// const toPath = (_path) => path.join(process.cwd(), _path)
+
 module.exports = {
-  "stories": [
-    "../src/components/**/stories.tsx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    // '../docs/*.stories.mdx',
+    '../src/components/**/stories.tsx',
+    '../src/DesignSystem/**/stories.tsx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    '@storybook/addon-essentials'
-  ]
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        modules: [path.resolve(__dirname, '..', 'src'), 'node_modules'],
+        alias: {
+          ...config.resolve.alias
+          // "@emotion/core": toPath("node_modules/@emotion/react"),
+          // "emotion-theming": toPath("node_modules/@emotion/react"),
+        }
+      }
+    }
+  }
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,12 @@
+import { CssBaseline } from '@material-ui/core'
+import { ThemeProvider } from '@material-ui/core/styles'
 
-import GlobalStyles from '../src/styles/global'
-export const decorators = [
-  (Story) => (
-    <>
-      <GlobalStyles />
-      <Story />
-    </>
-  ),
-];
+import { addDecorator } from '@storybook/react'
+import theme from 'styles/theme'
+
+addDecorator((story) => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    {story()}
+  </ThemeProvider>
+))
